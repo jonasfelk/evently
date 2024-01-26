e31aef95410ee06825151f8a96ec5060d612cdd4
Jonas Felk <felkjonas@gmail.com>
2024-01-26 12:06:41 +0600

    finish

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 // import CategoryFilter from '@/components/shared/CategoryFilter';
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 // import Search from '@/components/shared/Search';
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
@@ -55,10 +57,10 @@ export default async function Home({ searchParams }: SearchParamProps) {
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          {/* <Search />
-          <CategoryFilter /> */}
+          <Search />
+          <CategoryFilter />
         </div>
-
+        
         <Collection
           data={events?.data}
           emptyTitle="No Events Found"
